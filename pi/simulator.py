a27f1594efe132d3f31664a471f7115f7d50a7d6
Wadis3 <davidwadimp@gmail.com>
2026-04-29 12:34:24 +0200

    testing

--- pi/simulator.py
+++ pi/simulator.py
@@ -15,7 +15,7 @@ def deltaLatLng(meters, angle, current):
     x_meters = meters * math.cos(angle)
 
     d_long = (y_meters / 111111) * (-1 if y_meters > 0 else 1)
-    d_lat = x_meters / (111111 * math.cos(d_long + current[0])) * (-1 if x_meters > 0 else 1)
+    d_lat = x_meters / (111111 * math.cos(d_long + current[0])) * (-1 if x_meters < 0 else 1)
 
     return d_long, d_lat
 
