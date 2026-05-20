e8a48144a6342c657149aae87c2631232b5116aa
Jakob Agardh <jakobagardh61@gmail.com>
2026-05-20 15:16:15 +0200

    ahf

--- pi-car/car.py
+++ pi-car/car.py
@@ -24,35 +24,36 @@ coords = []
 @app.route('/', methods=['POST'])
 def main():
 	coords = request.json
-	
-	subprocess.Popen(["python3", "simulator.py", '--ip', myIP])
 
 	redis_server.set('long', coords[0])
 	redis_server.set('lat', coords[1])
-	while True:
-	    event = sense.stick.wait_for_event()
-	    if event.direction =="right":
-	        coords[0] = coords[0] + stepSize
+	
+	subprocess.Popen(["python3", "simulator.py", '--ip', myIP])
 
-	    elif event.direction == "left":
-	        coords[0] = coords[0] - stepSize
-
-	    elif event.direction == "down":
-	        coords[1] = coords[1] + stepSize
-
-	    elif event.direction == "up":
-	        coords[1] = coords[1] - stepSize
-
-	    with requests.Session() as session:
-	        car_info = {
-	            'IP': myIP,
-	            'longitude': coords[0],
-	            'latitude': coords[1]
-	        }
-	        resp = session.post(SERVER_URL, json=car_info)
-
-	    redis_server.set('long', coords[0])
-	    redis_server.set('lat', coords[1])
+#	while True:
+#	    event = sense.stick.wait_for_event()
+#	    if event.direction =="right":
+#	        coords[0] = coords[0] + stepSize
+#
+#	    elif event.direction == "left":
+#	        coords[0] = coords[0] - stepSize
+#
+#	    elif event.direction == "down":
+#	        coords[1] = coords[1] + stepSize
+#
+#	    elif event.direction == "up":
+#	        coords[1] = coords[1] - stepSize
+#
+#	    with requests.Session() as session:
+#	        car_info = {
+#	            'IP': myIP,
+#	            'longitude': coords[0],
+#	            'latitude': coords[1]
+#	        }
+#	        resp = session.post(SERVER_URL, json=car_info)
+#
+#	    redis_server.set('long', coords[0])
+#	    redis_server.set('lat', coords[1])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
